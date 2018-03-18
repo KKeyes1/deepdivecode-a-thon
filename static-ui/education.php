0d182327382b738b3893653b88ed52c5bd12b53d
Ken Keyes <kkeyes1@cnm.edu>
2018-03-18 09:29:47 -0600

    some very rough static ui/wireframe work to start thinking about angular components and views

---
+++ static-ui/education.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+		<title>Forum</title>
+	</head>
+	<body>
+		<div class="jumbotron">
+			<div class="container">
+
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<a class="navbar-brand" href="index.php">LOUDER!</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item active">
+								<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="communities.php">Communities</a>
+							</li>
+						</ul>
+						<form class="form-inline my-2 my-lg-0">
+							<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+							<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+						</form>
+					</div>
+				</nav>
+				<p class="lead">
+					<a class="btn btn-primary btn-lg" href="newpost.php" role="button">New Post</a>
+				</p>
+				<a href=""><div class="jumbotron">
+					<p>Post content</p>
+					<hr>
+					<p>- Profile</p>
+				</div></a>
+				<a href=""><div class="jumbotron">
+						<p>Post content</p>
+						<hr>
+						<p>- Profile</p>
+					</div></a>
+				<a href=""><div class="jumbotron">
+						<p>Post content</p>
+						<hr>
+						<p>- Profile</p>
+					</div></a>
+				<a href=""><div class="jumbotron">
+						<p>Post content</p>
+						<hr>
+						<p>- Profile</p>
+					</div></a>
+				<a href=""><div class="jumbotron">
+						<p>Post content</p>
+						<hr>
+						<p>- Profile</p>
+					</div></a>
+
+				<nav class="navbar fixed-bottom navbar-dark bg-primary">
+					<a class="navbar-brand" href="#!">&#9400; LOUDER!</a>
+				</nav>
+
+			</div>
+		</div>
+	</body>
+</html>
---
+++ static-ui/index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+		<title>Forum</title>
+	</head>
+	<body>
+		<div class="jumbotron">
+			<div class="container">
+
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<a class="navbar-brand" href="#">LOUDER!</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item active">
+								<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="communities.php">Communities</a>
+							</li>
+						</ul>
+						<form class="form-inline my-2 my-lg-0">
+							<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+							<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+						</form>
+					</div>
+				</nav>
+
+				<div class="jumbotron">
+					<h1 class="display-3">Get LOUD New Mexico! We're listening!</h1>
+					<p class="lead">Louder! is where you can share your story, ask for help, or lend a hand.</p>
+					<hr class="my-2">
+					<p>Learn more about your community, and get more involved with each other.</p>
+					<p class="lead">
+						<a class="btn btn-primary btn-lg" href="sign-up.php" role="button">Sign Up</a>
+					</p>
+				</div>
+
+				<div class="row">
+					<div class="col">
+						<div class="card">
+							<img class="card-img" src="nm.jpg" alt="Card image">
+							<a href="education.php"><div class="card-img-overlay">
+								<p class="card-text">Education</p>
+							</div></a>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card">
+							<img class="card-img" src="nm.jpg" alt="Card image">
+							<div class="card-img-overlay">
+								<p class="card-text">Income</p>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card">
+							<img class="card-img" src="nm.jpg" alt="Card image">
+							<div class="card-img-overlay">
+								<p class="card-text">Food Security</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<div class="card">
+							<img class="card-img" src="nm.jpg" alt="Card image">
+							<div class="card-img-overlay">
+								<p class="card-text">Community</p>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card">
+							<img class="card-img" src="nm.jpg" alt="Card image">
+							<div class="card-img-overlay">
+								<p class="card-text">Community</p>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card">
+							<img class="card-img" src="nm.jpg" alt="Card image">
+							<div class="card-img-overlay">
+								<p class="card-text">Community</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<div class="card">
+							<img class="card-img" src="nm.jpg" alt="Card image">
+							<div class="card-img-overlay">
+								<p class="card-text">Community</p>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card">
+							<img class="card-img" src="nm.jpg" alt="Card image">
+							<div class="card-img-overlay">
+								<p class="card-text">Community</p>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card">
+							<img class="card-img" src="nm.jpg" alt="Card image">
+							<div class="card-img-overlay">
+								<p class="card-text">Community</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<nav class="navbar fixed-bottom navbar-dark bg-primary">
+					<a class="navbar-brand" href="#!">&#9400; LOUDER!</a>
+				</nav>
+
+			</div>
+		</div>
+	</body>
+</html>
